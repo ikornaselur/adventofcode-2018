77a9a6d9a1083f324e348dc526fb32f3bb55809b
Axel Örn Sigurðsson <axel@absalon.is>
2018-12-14 19:32:07 +0000

    [Day1 Part2] Initial implementation

--- src/day1/main.rs
+++ src/day1/main.rs
@@ -1,5 +1,6 @@
 mod error;
 
+use std::collections::HashSet;
 use std::fs::File;
 use std::io::prelude::*;
 
@@ -18,11 +19,30 @@ fn main() -> Result<()> {
     // Part 1
     let contents = read_input("src/day1/input1.txt")?;
 
-    let result: i32 = contents
+    let parsed: Vec<i32> = contents
         .lines()
         .map(|line| line.parse::<i32>().unwrap())
-        .sum();
+        .collect();
+
+    let result: i32 = parsed.iter().sum();
     println!("Day 1 - Part 1: {}", result);
+
+    // Part 2
+    // Input remains the same
+    let mut sum: i32 = 0;
+    let mut seen = HashSet::new();
+
+    'outer: loop {
+        for value in parsed.iter() {
+            seen.insert(sum);
+
+            sum += value;
+            if seen.contains(&sum) {
+                break 'outer;
+            }
+        }
+    }
+    println!("Day 1 - Part 2: {}", sum);
 
     Ok(())
 }
