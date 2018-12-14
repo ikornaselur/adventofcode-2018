9e707078453e5b906477f8f12150458bb3fdb1a4
Axel Örn Sigurðsson <axel@absalon.is>
2018-12-14 20:26:51 +0000

    [Day2 Part2] Initial implementation

--- src/day2/main.rs
+++ src/day2/main.rs
@@ -15,6 +15,10 @@ fn read_input(path: &str) -> Result<String> {
     Ok(s)
 }
 
+/// Checks if a strings has the exact count of any character
+///
+/// This the string abbbcccd would return true for 1 or 3, but false for every thing else, since it
+/// has 1 a (and 1d) or 3 b (and 3 c).
 fn contains_char_count(line: &str, count: usize) -> bool {
     let mut counter = HashMap::new();
 
@@ -24,6 +28,15 @@ fn contains_char_count(line: &str, count: usize) -> bool {
     }
 
     counter.values().any(|&cnt| cnt == count)
+}
+
+/// Number of characters that differ between two stringso
+fn char_diff_count(first: &str, second: &str) -> usize {
+    first
+        .chars()
+        .zip(second.chars())
+        .filter(|(a, b)| a != b)
+        .count()
 }
 
 fn main() -> Result<()> {
@@ -42,11 +55,30 @@ fn main() -> Result<()> {
 
     println!("Day 2 - Part 1: {}", doubles * triples);
 
+    // Part 2
+    // Input remains the same
+
+    let mut result = String::new();
+    'outer: for (idx, line) in contents.lines().enumerate() {
+        for second_line in contents.lines().skip(idx + 1) {
+            if char_diff_count(line, second_line) == 1 {
+                result = line
+                    .chars()
+                    .zip(second_line.chars())
+                    .filter(|(a, b)| a == b)
+                    .map(|(a, _)| a)
+                    .collect();
+                break 'outer;
+            }
+        }
+    }
+    println!("Day 2 - Part 2: {}", result);
+
     Ok(())
 }
 
 #[cfg(test)]
-mod text {
+mod test {
     use super::*;
 
     #[test]
@@ -62,5 +94,20 @@ mod text {
     #[test]
     fn contains_char_count_with_multiple_doubles() {
         assert_eq!(contains_char_count("aabcdd", 2), true);
+    }
+
+    #[test]
+    fn char_diff_count_matches_same_strings() {
+        assert_eq!(char_diff_count("abcd", "abcd"), 0);
+    }
+
+    #[test]
+    fn char_diff_count_counts_different_characters() {
+        assert_eq!(char_diff_count("abcd", "axcd"), 1);
+    }
+
+    #[test]
+    fn char_diff_count_counts_different_characters_independently() {
+        assert_eq!(char_diff_count("abcd", "acbd"), 2);
     }
 }
